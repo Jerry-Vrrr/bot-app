3241682371cb916ee51470b2305fa1de798037cf
Abdul Hadi <abdul.806@yahoo.com>
2025-06-10 00:06:17 +0500

    host name changed again

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "aion-chatbot.s3.eu-north-1.amazonaws.com", // Your S3 bucket hostname
+        hostname: "integbot.s3.ap-southeast-2.amazonaws.com", // Your S3 bucket hostname
         pathname: "/images/**", // Allow images inside the "images" folder
       },
       
